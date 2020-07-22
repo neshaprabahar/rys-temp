f03a648bbf0a3f61fc54fa70ad1830ed836b6307
Nesha Prabahar <nprabahar3@gatech.edu>
2020-07-22 18:52:50 -0400

    basic table

---
+++ table-page/src/App.js
@@ -0,0 +1,75 @@
+import React, { useMemo, useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import Table from "./components/Table";
+
+function App() {
+
+  /* 
+    - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
+    - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
+  */
+
+  const [data, setData] = useState([]);
+
+  // Using useEffect to call the API once mounted and set the data
+  useEffect(() => {
+    (async () => {
+      const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
+      setData(result.data);
+    })();
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        // first group - TV Show
+        Header: "TV Show",
+        // First group columns
+        columns: [
+          {
+            Header: "Name",
+            accessor: "show.name"
+          },
+          {
+            Header: "Type",
+            accessor: "show.type"
+          }
+        ]
+      },
+      {
+        // Second group - Details
+        Header: "Details",
+        // Second group columns
+        columns: [
+          {
+            Header: "Language",
+            accessor: "show.language"
+          },
+          {
+            Header: "Genre(s)",
+            accessor: "show.genres"
+          },
+          {
+            Header: "Runtime",
+            accessor: "show.runtime"
+          },
+          {
+            Header: "Status",
+            accessor: "show.status"
+          }
+        ]
+      }
+    ],
+    []
+  );
+
+  return (
+    <div className="App">
+      <Table columns={columns} data={data} />
+    </div>
+  );
+}
+
+export default App
